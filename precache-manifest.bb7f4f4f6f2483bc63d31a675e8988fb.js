44cb67e0216f562ab8c190bb7723eb8c76c9f96c
Barry Walsh <barry.walsh@changingenergy.eu>
2020-09-14 23:34:07 +0100

    Updates

---
+++ precache-manifest.bb7f4f4f6f2483bc63d31a675e8988fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf46c7adea82cc9508e13fe0665467a0",
+    "url": "/DeepLearning/index.html"
+  },
+  {
+    "revision": "479ba7101ce8208cf57a",
+    "url": "/DeepLearning/static/css/main.9c647f67.chunk.css"
+  },
+  {
+    "revision": "4d6a52b836bd1cfaf66a",
+    "url": "/DeepLearning/static/js/2.84c98123.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/DeepLearning/static/js/2.84c98123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "479ba7101ce8208cf57a",
+    "url": "/DeepLearning/static/js/main.ae3cba92.chunk.js"
+  },
+  {
+    "revision": "4e07e83a1c21bb9522a8",
+    "url": "/DeepLearning/static/js/runtime-main.66719ad0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DeepLearning/static/media/logo.5d5d9eef.svg"
+  }
+]);
